5402a504ee98d55e3253a35e7a541273258f6a04
Aaron Eshbach <aaron.eshbach@hotmail.com>
2021-03-23 11:54:05 -0400

    Add agent package

---
+++ CurryOn/CurryOn.Agent/Pool.fs
@@ -0,0 +1,94 @@
+﻿namespace CurryOn.Agent
+
+open FSharpx.Collections
+
+type private PoolMessage<'a> =
+| Acquire of AsyncReplyChannel<'a>
+| Release of 'a
+
+type private PoolState<'a> =
+    {
+        TotalCount: int
+        Available: 'a list
+        Waiting: Queue<AsyncReplyChannel<'a>>
+    }
+
+/// A pool of re-usable objects with Async acquire and release functions
+type ObjectPool<'a>(generate: unit -> 'a, initialCount, maxCount) =
+    let pool = 
+        { 
+            TotalCount = initialCount
+            Available = List.init initialCount <| fun _ -> generate()
+            Waiting = Queue.empty
+        }        
+
+    let agent = 
+        MailboxProcessor.Start <| fun inbox ->
+            let rec dispatch (pool: PoolState<'a>) = 
+                async {
+                    let! request = inbox.Receive()
+                    match request with
+                    | Acquire waiter ->
+                        match pool.Available with
+                        | head::tail ->
+                            waiter.Reply(head)
+                            return! dispatch { pool with Available = tail }
+                        | [] ->
+                            if pool.TotalCount < maxCount then 
+                                let newTotal = pool.TotalCount + 1
+                                waiter.Reply <| generate()
+                                return! dispatch { pool with TotalCount = newTotal }
+                            else 
+                                return! dispatch { pool with Waiting = pool.Waiting.Conj(waiter) }
+                    | Release value ->
+                        match pool.Waiting |> Queue.tryUncons with
+                        | Some (waiter, queue) ->
+                            waiter.Reply(value)
+                            return! dispatch { pool with Waiting = queue }
+                        | None ->
+                            return! dispatch { pool with Available = value :: pool.Available }
+                }
+            dispatch pool
+
+    /// Return an item into the pool
+    member __.Release (item) =
+        agent.Post (Release item)
+
+    /// Gets an item from the pool if available,
+    /// or if there are none present either uses the generator if 
+    /// not already at the maximum, or waits for an object to be released
+    member __.Acquire () =
+        agent.PostAndReply(Acquire)
+
+    /// Asynchronously gets an item from the pool if available,
+    /// or if there are none present either uses the generator if 
+    /// not already at the maximum, or waits for an object to be released
+    member __.AcquireAsync () =
+        agent.PostAndAsyncReply(Acquire)
+
+
+type PoolRouterMessage<'resource> =
+| ProcessRequest of ('resource -> obj) * AsyncReplyChannel<obj>
+
+type PoolRouterAgent<'resource> (routerType, newResource) =
+    let router =
+        routerType |> RouterAgent.start<PoolRouterMessage<'resource>>
+            (fun _ inbox ->
+                let rec loop resource =
+                    async {
+                        let! message = inbox.Receive()
+                        match message with
+                        | ProcessRequest (f, channel) ->
+                            let result = f resource
+                            channel.Reply(result)
+                            return! loop resource
+                    }
+                let resource = newResource()
+                loop resource)
+
+    member __.Execute<'result> (request: 'resource -> 'result) =
+        async {
+            let! response = router.PostAndAsyncReply(fun channel -> ProcessRequest (request >> box, channel))
+            return response |> unbox<'result>
+        }
+
---
+++ CurryOn/CurryOn.Agent/Router.fs
@@ -0,0 +1,63 @@
+﻿namespace CurryOn.Agent
+
+type RouterType<'message> =
+| RoundRobin of int
+| SmallestMailbox of int
+| ConsistentHashing of (int * ('message -> int))
+
+type private Routee<'message> =
+    {
+        Index: int
+        Actor: MailboxProcessor<'message>
+    }
+
+type RouterAgent<'message> (routerType, f) =
+    let createPool poolSize = 
+        [| 1 .. poolSize |] |> Array.map (fun index -> MailboxProcessor<'message>.Start (f index))
+
+    let getRoutee =
+        match routerType with
+        | RoundRobin poolSize ->
+            let routees = createPool poolSize
+            fun current _ ->
+                if current >= routees.Length - 1
+                then { Index = 0; Actor = routees.[0] }
+                else { Index = current+1; Actor = routees.[current + 1] }
+        | SmallestMailbox poolSize ->
+            let routees = createPool poolSize
+            fun _ _ ->
+                let routee = routees |> Seq.sortBy (fun routee -> routee.CurrentQueueLength) |> Seq.head
+                { Index = 0; Actor = routee }
+        | ConsistentHashing (poolSize, hash) ->
+            let routees = createPool poolSize
+            fun _ message ->
+                let index = 
+                    let computedHash = hash message
+                    computedHash % poolSize
+                { Index = index; Actor = routees.[index] }
+
+    let router = 
+        MailboxProcessor<'message>.Start
+        <| fun inbox ->
+            let rec loop currentRoutee =
+                async {
+                    let! message = inbox.Receive()
+                    let routee = message |> getRoutee currentRoutee
+                    routee.Actor.Post(message)
+                    return! routee.Index |> loop
+                }
+            loop 0
+
+    member __.Post message =
+        router.Post(message)
+
+    member __.PostAndReply f =
+        router.PostAndReply(f)
+
+    member __.PostAndAsyncReply f =
+        router.PostAndAsyncReply(f)
+
+
+module RouterAgent =
+    let start<'message> f routerType =
+        new RouterAgent<'message>(routerType, f)
