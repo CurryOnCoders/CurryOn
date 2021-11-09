dee8dea27862e4169360c0c48d33104eb7545ea0
Aaron Eshbach <aaron.eshbach@hotmail.com>
2021-11-09 16:40:57 -0500

    Added license information

--- scripts/MovePackages.fsx
+++ scripts/MovePackages.fsx
@@ -11,11 +11,19 @@ let copy sideloadingFolder (package: FileInfo) =
     with ex ->
         printfn "Error moving package %s: %s" package.Name ex.Message
 
-let copyAll root target =
+let copyDebug root target =
     getPackages root
+    |> Seq.filter (fun p -> p.FullName.Contains("Debug"))
     |> Seq.iter (copy target)
+
+let copyRelease root target =
+    getPackages root
+    |> Seq.filter (fun p -> p.FullName.Contains("Release"))
+    |> Seq.iter (copy (sprintf "%s\\release" target))
+    
 
 let root = @"C:\repos\CurryOn\CurryOn"
 let target = @"C:\nuget"
 
-copyAll root target
+//copyDebug root target
+copyRelease root target
